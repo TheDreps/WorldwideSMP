a897475be1a0ee2de898d38934c68d3c15748768
Lewis Mercer <lewismercer@hotmail.co.uk>
2021-05-12 03:14:50 +0100

    Initial commit

---
+++ src/main/java/com/worldwidesmp/worldwidesmp/WorldwideSMP.java
@@ -0,0 +1,17 @@
+package com.worldwidesmp.worldwidesmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldwideSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
